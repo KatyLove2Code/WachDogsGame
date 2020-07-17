fdcff3869fab1a03f087eacb0769c82e636c4f2f
ViktorTum <vicktortumilevich@gmail.com>
2020-07-17 21:57:29 +1000

    Revert "Merge branch 'master' of https://github.com/KatyLove2Code/WachDogsGame"
    This reverts commit 5ef6a30159eb64328f5e69b324d65c962d958b75, reversing
    changes made to 7b5a99997b255124a7b45f894788051a3b4c1edc.

--- game1.py
+++ game1.py
@@ -7,8 +7,7 @@ pygame.init()
 win = pygame.display.set_mode((500, 500))
 
 pygame.display.set_caption("Mario")
-padOn = False
-keyboard = True
+
 x = 50
 y = 425
 width = 40
@@ -19,16 +18,13 @@ isJump = False
 jumpCount = 10
 pygame.joystick.init()
 
-try:
-	my_joystick = pygame.joystick.Joystick(0)
-	my_joystick.init()
-	joysticks = []
-	for i in range(0, pygame.joystick.get_count()):
-		joysticks.append(pygame.joystick.Joystick(i))
-		joysticks[-1].init()
-except:
-	print('Joystick not found')
-	padOn = False
+my_joystick = pygame.joystick.Joystick(0)
+my_joystick.init()
+
+joysticks = []
+for i in range(0, pygame.joystick.get_count()):
+	joysticks.append(pygame.joystick.Joystick(i))
+	joysticks[-1].init()
 
 run = True
 while  run:
@@ -38,69 +34,47 @@ while  run:
 		if event.type == pygame.QUIT:
 			run = False
 
+
+	if my_joystick.get_hat(0) == (-1, -1):
+		x -= speed
+		y += speed
+	if my_joystick.get_hat(0) == (1, 1):
+		x += speed
+		y -= speed
+	if my_joystick.get_hat(0) == (-1, 1):
+		x -= speed
+		y -= speed
+	if my_joystick.get_hat(0) == (1, -1):
+		x += speed
+		y += speed
+
+
 	keys = pygame.key.get_pressed()
+	if keys[pygame.K_LEFT] or my_joystick.get_hat(0) == (-1, 0) and x > 5:
+		x -= speed
 
-	if keys[pygame.K_p] and padOn == False:
-		padOn = True
-		keyboard = False
-		print("True")
-
-	if keys[pygame.K_o] and padOn == True:
-		padOn = False
-		keyboard = True
-		print("False")
-	if padOn == True:
-
-		if my_joystick.get_hat(0) == (-1, -1) and x > 5 and y < 500 - height - 15:
-			x -= speed
-			y += speed
-		if my_joystick.get_hat(0) == (1, 1) and x < 500	 - width - 5 and y > 5:
-			x += speed
+	if keys[pygame.K_RIGHT] or my_joystick.get_hat(0) == (1, 0) and x < 500	 - width - 5:
+		x += speed
+	if not(isJump):
+		if keys[pygame.K_UP] or my_joystick.get_hat(0) == (0, 1) and y > 5:
 			y -= speed
-		if my_joystick.get_hat(0) == (-1, 1) and x > 5 and y > 5:
-			x -= speed
-			y -= speed
-		if my_joystick.get_hat(0) == (1, -1) and x < 500	 - width - 5 and y < 500 - height - 15:
-			x += speed
+		if keys[pygame.K_DOWN] or my_joystick.get_hat(0) == (0, -1) and y < 500 - height - 15:
 			y += speed
-		if my_joystick.get_hat(0) == (-1, 0) and x > 5:
-			x -= speed
-		if my_joystick.get_hat(0) == (1, 0) and x < 500	 - width - 5:
-			x += speed
-		if  my_joystick.get_hat(0) == (0, 1) and y > 5:
-			y -= speed
-		if my_joystick.get_hat(0) == (0, -1) and y < 500 - height - 15:
-			y += speed
-
-
-
-	if keyboard == True:
-
-		if keys[pygame.K_LEFT] and x > 5:
-			x -= speed
-
-		if keys[pygame.K_RIGHT] and x < 500	 - width - 5:
-			x += speed
-		if not(isJump):
-			if keys[pygame.K_UP] and y > 5:
-				y -= speed
-			if keys[pygame.K_DOWN] and y < 500 - height - 15:
-				y += speed
-			if keys[pygame.K_SPACE]:
-				isJump = True
+		if keys[pygame.K_SPACE]:
+			isJump = True
+	else:
+		if jumpCount >= - 10:
+			y -= (jumpCount ** 2) / 2
+			jumpCount -= 1
 		else:
-			if jumpCount >= - 10:
-				y -= (jumpCount ** 2) / 2
-				jumpCount -= 1
-			else:
-				isJump = False
-				jumpCount = 10
+			isJump = False
+			jumpCount = 10
 
 
 
 	win.fill((0,0,0))
 	pygame.draw.rect(win, (0, 0, 255), (x, int(y), width, height))
-	pygame.display.update()
+	pygame.display.update()	
 
 
-pygame.quit()
+pygame.quit()			
