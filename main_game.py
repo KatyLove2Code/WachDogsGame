2840058a581f46a54c5ca75d96a60da287554baa
Ekaterina Divina <ekaterinadivina@gmail.com>
2020-07-17 09:58:04 +0300

    start game

---
+++ main_game.py
@@ -0,0 +1,16 @@
+import pygame
+pygame.init()
+
+maindisplay = pygame.display.set_mode((800, 600))
+
+
+game = True
+while game:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            game = False
+    maindisplay.fill((124,89, 194))
+    pygame.display.update()
+
+pygame.quit()
+print("Katy Divina")
