3ffd8e0649a725ee57bd1d01ab09e040452ebe24
KatyDivina <ekaterinadivina@gmail.com>
2020-08-05 09:35:19 +0300

    attac anim

--- classHero.py
+++ classHero.py
@@ -145,7 +145,9 @@ class Hero(pygame.sprite.Sprite):
             self.speedY = 0
 
 
-        if keys[pygame.K_e] and self.attack == False  and self.speedX == 0: #атака
+        if keys[pygame.K_e] and self.speedX == 0: #атака
+            if self.attack == False:
+                self.animCount = 0
             self.attack = True
         else:
             self.attack = False
@@ -194,28 +196,38 @@ class Hero(pygame.sprite.Sprite):
         :return:
         '''
 
-        if self.speedX != 0:  # Если скорость по Х не нулевая, значит я иду
-            self.animCount += 1  # Счётчик подсчитывает, какую картинку по счёту я должен показать
-            if self.animCount == len(runAnimation):  # если я дошёл до последней картинки в списке картинок
+
+
+        #ATAKA
+        if self.attack == True:
+            self.animCount += 1   # Для ускоренной анимации атаки
+            if self.animCount >= len(attackAnimation):    # если я дошёл до последней картинки в списке картинок
+                self.animCount = 0        # то обнуляю счётчик, чтобы начать сначала
+            self.image = attackAnimation[self.animCount]    # Достаю картинку с нужным номером из списка
+
+        #БЕГ
+        elif self.speedX != 0:  # Если скорость по Х не нулевая, значит я иду
+            if self.animCount >= len(runAnimation):  # если я дошёл до последней картинки в списке картинок
                 self.animCount = 0  # то обнуляю счётчик, чтобы начать сначала
 
             self.image = runAnimation[self.animCount]  # Достаю картинку с нужным номером из списка
-            if self.speedX > 0:  # Если двигаюсь вправо,
-                self.image = pygame.transform.flip(self.image, True, False)  # то отзеркаливаю картинку персонажа
+            # if self.speedX > 0:  # Если двигаюсь вправо,
+            #     self.image = pygame.transform.flip(self.image, True, False)  # то отзеркаливаю картинку персонажа
 
-        if self.speedX == 0:  # иначе скорость = 0, значит стою на месте
-            self.animCount += 1
-            if self.animCount == len(idleAnimation):
+        #СТОИТ
+        elif self.speedX == 0:  # иначе скорость = 0, значит стою на месте
+            if self.animCount >= len(idleAnimation):
                 self.animCount = 0
             self.image = idleAnimation[self.animCount]
 
-            if self.idleLeft == False:
-                self.image = pygame.transform.flip(self.image, True, False)
+        #Если направление вправо, то зеркалим картинку
+        if self.idleLeft == False:
+           self.image = pygame.transform.flip(self.image, True, False)
+
+        self.animCount += 1  # Счётчик подсчитывает, какую картинку по счёту я должен показать
 
 
-        if self.attack == True:
-            self.animCount += 2
-            if self.animCount == len(attackAnimation):
-                self.animCount = 0
-            self.image = attackAnimation[self.animCount]
+
+
+
 
